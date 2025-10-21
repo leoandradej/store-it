5a955dbc015d1805343f46988e35d4a68cb09aae
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-10-20 23:36:47 -0300

    Added Appwrite setup and server action to create an account

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,70 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listRows({
+    databaseId: appwriteConfig.databaseId,
+    tableId: appwriteConfig.usersTableId,
+    queries: [Query.equal("email", [email])],
+  });
+
+  return result.total > 0 ? result.rows[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+export const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken({
+      userId: ID.unique(),
+      email: email,
+    });
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error("Failed to send an OTP");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createRow({
+      databaseId: appwriteConfig.databaseId,
+      tableId: appwriteConfig.usersTableId,
+      rowId: ID.unique(),
+      data: {
+        fullName,
+        email,
+        avatar: "/assets/images/avatar.png",
+        accountId,
+      },
+    });
+  }
+
+  return parseStringify({ accountId });
+};
